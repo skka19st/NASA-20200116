e1804e8cba382ce83a748915d534846601441329
Fredrik Byström <fredbstrom@gmail.com>
2020-01-16 15:59:07 +0100

    Added fetch

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-
 fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=0aQk19tdETuiM8F0sa5NCmVwNGTHILMnPWp9lO0j")
 .then(function(response){return response.json()})
 .then(function(response) {
+    
+    rover = response;
+    //console.log(rover.photos[i].img_src);
 
-  console.log(response);
+    let img = "";
+    for (i = 0; i < rover.photos.length; i++){
+        
+        
+        img += "<img src='" + rover.photos[i].img_src + "'>";
+        document.getElementById("rover-image").innerHTML = img;
+    }
+    
 })
 
 .catch(function(response) {
