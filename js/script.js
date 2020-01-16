3444fea082ee26a01674bfeecce88212570434fb
Fredrik Byström <fredbstrom@gmail.com>
2020-01-16 18:33:07 +0100

    changed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=0aQk19tdETuiM8F0sa5NCmVwNGTHILMnPWp9lO0j")
-.then(function(response){return response.json()})
-.then(function(response) {
-    
-    rover = response;
-    //console.log(rover.photos[i].img_src);
 
-    let img = "";
-    for (i = 0; i < rover.photos.length; i++){
+function inputDate(){
+  //let dateOnMars = document.getElementById("id");
+  let dateOnMars = "2016-05-29"; // här ska datumet vara
+
+  fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + dateOnMars + "&api_key=0aQk19tdETuiM8F0sa5NCmVwNGTHILMnPWp9lO0j")
+  .then(function(response){return response.json()})
+  .then(function(response) {
+      
+      rover = response;
+      
+      console.log(response);
+
+      if(rover.photos.length <= 0){
+        document.getElementById("rover-image").innerHTML = "No image!";
+      }else {
         
+      let caruselLoop = "";
+      let x = rover.photos.length;
         
-        img += "<img src='" + rover.photos[i].img_src + "'>";
-        document.getElementById("rover-image").innerHTML = img;
+      if (x > 9){
+        x = 9;
+      }
+
+      for (i = 0; i < x; i++){
+          console.log(rover.photos, i, rover.photos.length)
+          //// här ska koden fär karusellen ligga
+
+          caruselLoop += "<img src='" + rover.photos[i].img_src + "'>";
+          document.getElementById("rover-image").innerHTML = caruselLoop;
+      }
     }
-    
-})
+  })
 
-.catch(function(response) {
+  .catch(function(response) {
 
-  console.log('Error! Please try again');
-});
-
+    console.log('Error! Please try again', response);
+  });
+}
