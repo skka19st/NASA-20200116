31c7d41e8dc639ceeb35bf1112b9283af6413000
Fredrik Byström <fredbstrom@gmail.com>
2020-01-17 08:35:36 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
 function inputDate(){
+  let roverOnMars = document.getElementById("rover").value;
   let dateOnMars = document.getElementById("date").value;
   //let dateOnMars = "2016-05-29"; // här ska datumet vara
 
-  fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + dateOnMars + "&api_key=0aQk19tdETuiM8F0sa5NCmVwNGTHILMnPWp9lO0j")
+  fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverOnMars + "/photos?earth_date=" + dateOnMars + "&api_key=0aQk19tdETuiM8F0sa5NCmVwNGTHILMnPWp9lO0j")
   .then(function(response){return response.json()})
   .then(function(response) {
       
@@ -11,6 +12,8 @@ function inputDate(){
       
       console.log(response);
 
+      document.getElementById("show-rover-name").innerHTML = roverOnMars;
+      
       if(rover.photos.length <= 0){
         document.getElementById("rover-image").innerHTML = "No image!";
       }else {
@@ -21,7 +24,7 @@ function inputDate(){
       if (x > 9){
         x = 9;
       }
-
+      
       for (i = 0; i < x; i++){
           console.log(rover.photos, i, rover.photos.length)
           //// här ska koden fär karusellen ligga
